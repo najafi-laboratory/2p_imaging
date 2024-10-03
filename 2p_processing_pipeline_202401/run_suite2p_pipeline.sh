33ef222468fd6fa554ecab7ae5de483a75812c7d
TimStamm <90421614+TimStamm@users.noreply.github.com>
2024-10-03 16:49:44 -0400

    new branch

--- 2p_processing_pipeline_202401/run_suite2p_pipeline.sh
+++ 2p_processing_pipeline_202401/run_suite2p_pipeline.sh
@@ -7,15 +7,15 @@
 #SBATCH --mem-per-cpu=256G
 #SBATCH --time=6:00:00
 #SBATCH --output=Report_%A-%a.out
-#SBATCH --mail-user=hilberthuang05@gatech.edu
+#SBATCH --mail-user=gtg424h@gatech.edu
 
-cd /storage/coda1/p-fnajafi3/0/yhuang887/Projects/2p_processing_pipeline_202401
+cd /storage/coda1/p-fnajafi3/0/gtg424h/Projects/Pupil_Dilation/2p_processing_pipeline_202401
 source activate suite2p
 python run_suite2p_pipeline.py \
 --denoise 0 \
 --spatial_scale 1 \
---data_path './testdata/C2' \
---save_path './results/C2' \
+--data_path '/storage/cedar/cedar0/cedarp-fnajafi3-0/2p_imaging/FN13/20240618/FN13_P_20240618_js_DCNCNO_t-024' \
+--save_path './results/FN13_P_20240618_js_DCNCNO_t' \
 --nchannels 2 \
 --functional_chan 2 \
---brain_region 'ppc' \
+--brain_region 'crbl' \
