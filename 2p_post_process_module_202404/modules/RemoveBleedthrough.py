0b8ac1484f84a9bd5a4dda9eaa2e7887dc9c6030
iamaray <arya.nahri@gmail.com>
2024-10-04 16:59:39 -0400

    bleedthrough changed to multivar

--- 2p_post_process_module_202404/modules/RemoveBleedthrough.py
+++ 2p_post_process_module_202404/modules/RemoveBleedthrough.py
@@ -16,20 +16,38 @@ from .LabelExcInh import *
 def train_reg_model(ops, mean_anat, mean_func, labels):
     # multivariate regression on ROIs
 
-    # first, identify anat ROIs with labels --> r_i
-    # the same pixels in mean_func --> g_i
+    coords = np.where(labels == 1)
 
-    # get list of pixel coordinates
+    rows = coords[:, 0]
+    cols = coords[:, 1]
+    anat_roi_pixels = mean_anat[rows, cols]  # r_i
+    func_roi_pixels = mean_func[rows, cols]  # g_i
+
+    anat_roi_pixels = np.array([np.array([p]) for p in anat_roi_pixels])
+    func_roi_pixels = np.array([np.array([p]) for p in func_roi_pixels])
+
     # fit multivariate regression r_i = m_i * g_i + b_i
+    model = LinearRegression()
+
+    model.fit(func_roi_pixels, anat_roi_pixels)
+
+    # Get the slopes (coefficients) and intercepts for each variate in r
+    slopes = model.coef_.tolist()
+    offsets = model.intercept_.tolist()
 
     # return list of slopes, offsets, coordinates
-    pass
+    return slopes, offsets, coords
 
 
 def remove_green_bleedthrough(offsets, slopes, mean_func, mean_anat, coordinates):
     # for each coordinate
     # mean_anat_corrected = mean_anat
-    # mean_anat_corrected(coordinate) -= (mean * mean_func(coordinate) + offset)
+    mean_anat_corrected = mean_anat
+    # mean_anat_corrected(coordinate) -= (slope * mean_func(coordinate) + offset)
+
+    for i, c in enumerate(coordinates):
+        mean_anat_corrected[c] = mean_anat_corrected[c] - \
+            (slopes[i] * mean_func[c] + offsets[i])
 
     # return mean_anat_corrected
-    pass
+    return mean_anat_corrected
