5f6cec7d63eed920cf02616c8e7286d05cc86bc9
iamaray <arya.nahri@gmail.com>
2024-10-04 16:38:53 -0400

    small changes

--- 2p_post_process_module_202404/modules/RemoveBleedthrough.py
+++ 2p_post_process_module_202404/modules/RemoveBleedthrough.py
@@ -13,16 +13,12 @@ from sklearn.linear_model import LinearRegression
 from .LabelExcInh import *
 
 
-def fit_reg_for_roi(mean_anat_pixel, mean_func_pixel):
-    pass
-
-
 def train_reg_model(ops, mean_anat, mean_func, labels):
     # multivariate regression on ROIs
-    
+
     # first, identify anat ROIs with labels --> r_i
     # the same pixels in mean_func --> g_i
-    
+
     # get list of pixel coordinates
     # fit multivariate regression r_i = m_i * g_i + b_i
 
@@ -33,8 +29,7 @@ def train_reg_model(ops, mean_anat, mean_func, labels):
 def remove_green_bleedthrough(offsets, slopes, mean_func, mean_anat, coordinates):
     # for each coordinate
     # mean_anat_corrected = mean_anat
-    # mean_anat_corrected(coordinate) = (mean * mean_func(coordinate) + offset)
-
+    # mean_anat_corrected(coordinate) -= (mean * mean_func(coordinate) + offset)
 
     # return mean_anat_corrected
     pass
